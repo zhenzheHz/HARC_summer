648925f9a888eb2cacd549cc55f26c80de34dd00
zhenzheHz <109124xdf@gmail.com>
2024-07-08 09:33:15 +0800

    add d190.cpp

---
+++ d190.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define int int64_t
+using namespace std;
+int32_t main(){
+    int n;
+    while(cin >> n){
+        ios_base::sync_with_stdio(false);cin.tie(0);
+        vector<int> s(n);
+        for(int &i : s)cin >> i;
+        sort(s.begin(),s.end());
+        for(int &i : s)cout << i << ' ';
+        cout << '\n';
+    }
+}
